f0f6f6392cb5a83ef16c5ee0bf992ecbb2da4fe2
unknown <kukoc.luka@gmail.com>
2021-09-11 15:21:24 +0200

    fixed the error(else if statment was incorrect)

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,9 +4,10 @@ const geocode = (cityname, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + cityname + ".json?access_token=pk.eyJ1IjoibHVrYWt1a29jIiwiYSI6ImNrc3VnOHlycjAyMDgybnB0bXhhNHVzZ3AifQ._k0HUWhtN2AxjquwVaDJ5w"
 
     request({ url, json: true }, (error, {body}) => {
+        
         if(error) {
             callback("Unable to connect to location services!", undefined)
-        } else if (body.features.lenght === 0) {
+        } else if (!body.features.lenght) {
             callback("Unable to find location. Try another search", undefined)
         } else {
             callback(undefined, {
