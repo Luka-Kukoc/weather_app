dd384e99a5a96b1a7fe9b605325c583daf76f661
unknown <kukoc.luka@gmail.com>
2021-09-24 16:37:11 +0200

    fixes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,10 +4,11 @@ const geocode = (cityname, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + cityname + ".json?access_token=pk.eyJ1IjoibHVrYWt1a29jIiwiYSI6ImNrc3VnOHlycjAyMDgybnB0bXhhNHVzZ3AifQ._k0HUWhtN2AxjquwVaDJ5w"
 
     request({ url, json: true }, (error, {body}) => {
-        
+        console.log(body.features)
         if(error) {
             callback("Unable to connect to location services!", undefined)
-        } else if (!body.features.lenght) {
+        } else if(!body.features[0]) {
+            
             callback("Unable to find location. Try another search", undefined)
         } else {
             callback(undefined, {
